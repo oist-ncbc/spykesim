5c44bdf4d3107e56a6fd9ee8508a9458193b9151
Keita F. Watanabe <keitaw09@gmail.com>
2018-06-19 11:07:29 +0900

    modify default setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='spykesim',
+    version='0.0.0',
+    description='Python module that offers functions for measuring the similarity between two segmented multi-neuronal spiking activities.',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Keita Watanabe',
+    author_email='keitaw09@gmail.com',
+    install_requirements=['numpy', 'cython'],
+    url='https://github.com/KeitaW/spikesim',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
