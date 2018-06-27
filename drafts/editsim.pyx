cebdd2c46e05ffb6906bbcf06a3c698dd039aa67
Keita F. Watanabe <keitaw09@gmail.com>
2018-06-27 17:18:47 +0900

    add functions

--- drafts/editsim.pyx
+++ drafts/editsim.pyx
@@ -70,3 +70,62 @@ def csimpleeditsim(DBL_C [:, :] mat1, DBL_C [:, :] mat2):
                 dp_table[col1 + 1, col2],
                 dp_table[col1, col2] + match)
     return dp_table[-1, -1]
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(True)  # turn off negative index wrapping for entire function        
+def csimpleeditsim_withbp(DBL_C [:, :] mat1, DBL_C [:, :] mat2):
+    cdef int nrow, ncol, nneuron
+    cdef int col1, col2, row    
+    nrow = mat1.shape[1]
+    ncol = mat2.shape[1]
+    nneuron = mat1.shape[0]
+    cdef np.ndarray[DBL_C, ndim=2] dp_table = np.zeros((nrow+1, ncol+1), dtype = DBL)
+    cdef np.ndarray[DBL_C, ndim=2] bp_table = np.ones_like(dp_table, dtype = DBL) * (-1)
+    cdef DBL_C match
+    for col1 in range(nrow):
+        for col2 in range(ncol):
+            match = 0
+            for row in range(nneuron):
+                match += mat1[row, col1] * mat2[row, col2]            
+            pair = indmax(
+                dp_table[col1, col2 + 1],
+                dp_table[col1 + 1, col2],
+                dp_table[col1, col2] + match
+            )
+            bp_table[col1 + 1, col2 + 1] = pair.idx
+            dp_table[col1 + 1, col2 + 1] = pair.value
+    return dp_table[-1, -1], bp_table
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(True)  # turn off negative index wrapping for entire function        
+def csimpleeditsim_align(bp, mat1, mat2):
+    cdef int nrow, ncol, nneuron
+    cdef int col1, col2, row, col
+    nrow = bp.shape[0]
+    ncol = bp.shape[1]
+    row = nrow - 1
+    col = ncol - 1
+    # The first column is inserted just to avoid initialization error that may occur on concatination.
+    cdef np.ndarray[DBL_C, ndim=2] alignment1 = np.zeros((mat1.shape[0], 1), dtype = DBL)
+    cdef np.ndarray[DBL_C, ndim=2] alignment2 = np.zeros((mat1.shape[0], 1), dtype = DBL)
+    cdef np.ndarray[DBL_C, ndim=2] zerovec = np.zeros(mat1.shape[0], dtype = DBL) # which is corresponding to the null character.
+    while True:
+        if bp[row, col] == -1:
+            # Eather of the strings tracing terminated
+            break
+        elif bp[row, col] == 2:
+            alignment1 = np.c_[mat1[:, row - 1] * mat2[:, col - 1], alignment1]
+            alignment2 = np.c_[mat1[:, row - 1] * mat2[:, col - 1], alignment2]
+            row -= 1
+            col -= 1
+        elif bp[row, col] == 1:
+            alignment1 = np.c_[zerovec, alignment1]
+            alignment2 = np.c_[mat2[:, col - 1], alignment2]
+            col -= 1
+        elif bp[row, col] == 0:
+            alignment1 = np.c_[mat1[:, row - 1], alignment1]
+            alignment2 = np.c_[zerovec, alignment2]
+            row -= 1
+    return alignment1[:, :-1], alignment2[:, :-1]
+
+
