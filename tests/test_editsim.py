cf350c8319c1a5fae78caf9de2cca60943f7f90c
Keita F. Watanabe <keitaw09@gmail.com>
2019-03-01 01:18:20 +0900

    fix wrong path

---
+++ tests/test_editsim.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from nose.tools import ok_, eq_
+from ..spykesim.spykesim import editsim
+
+class SimmatTestCase(TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_genidvec(self):
+        eq_(1, 1)
