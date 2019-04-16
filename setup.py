8cec6344b1efe0d033e5672bffc8d30d6a441fc7
Keita F. Watanabe <keitaw09@gmail.com>
2019-04-16 10:25:59 +0900

    add doc string

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ cmdclass = {'build_ext': build_ext}
 import numpy
 setup(
     name='spykesim',
-    version='1.1.9',
+    version='1.1.10',
     description='Python module that offers functions for measuring the similarity between two segmented multi-neuronal spiking activities.',
     long_description=readme,
     author='Keita Watanabe',
